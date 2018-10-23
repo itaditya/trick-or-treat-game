402975ac0211df2ff068d819383daf0ac5edbd74
Aditya Agarwal <adityaa803@gmail.com>
2018-10-23 08:37:23 +0530

    added basic react-testing-library test

--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -1,3 +1,16 @@
-test("testing in browser works", () => {
+import React from "react";
+import { render } from "react-testing-library";
+
+import { Game } from "../Game";
+
+test("react-testing-library works", () => {
   expect(1 + 2).toBe(3);
+  const boardSizeX = 10;
+  const boardSizeY = 10;
+  const { getByTestId, container } = render(
+    <Game boardSizeX={boardSizeX} boardSizeY={boardSizeY} />
+  );
+
+  const gameTable = getByTestId("game-table");
+  expect(gameTable.children.length).toBe(1);
 });
