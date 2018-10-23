5a2198611f49c84451d5f1949e35a175bbfabd69
Aditya Agarwal <adityaa803@gmail.com>
2018-10-23 08:08:04 +0530

    removed prompt

--- src/index.js
+++ src/index.js
@@ -5,19 +5,15 @@ import "./styles.css";
 import { Game } from "./Game";
 
 const App = function() {
-  const boardSizeX = parseInt(prompt("Enter boardSizeX"));
-  const boardSizeY = parseInt(prompt("Enter boardSizeY"));
-  if (Number.isNaN(boardSizeX) || Number.isNaN(boardSizeY)) {
-    alert("Please provide proper values");
-    return;
-  }
+  const boardSizeX = 10;
+  const boardSizeY = 10;
 
   return (
     <div className="App">
       <Game boardSizeX={boardSizeX} boardSizeY={boardSizeY} />
     </div>
   );
-}
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
