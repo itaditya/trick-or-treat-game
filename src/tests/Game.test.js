9219a71fac259bcaae10d93bf8a128767f533dc3
Paweł Kochanek <pawel.kochanek@gmail.com>
2018-10-24 23:20:00 +0200

    More refactoring

--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -1,16 +1,14 @@
 import React from "react";
-// add some helpful assertions
 import 'jest-dom/extend-expect'
-// this is basically: afterEach(cleanup)
 import 'react-testing-library/cleanup-after-each'
-import { render } from "react-testing-library";
-
+import { render, queryByAttribute } from "react-testing-library";
 import { Game } from "../Game";
+
+const boardSizeX = 10;
+const boardSizeY = 10;
 
 describe('Game board', () => {
   it('should render correctly', () => {
-    const boardSizeX = 10;
-    const boardSizeY = 10;
     const { getByTestId } = render(
       <Game boardSizeX={boardSizeX} boardSizeY={boardSizeY} />
     );
