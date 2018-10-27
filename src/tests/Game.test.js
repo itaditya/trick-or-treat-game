efa5f454fc5b7923efc3d9ecaf10b608105fe2e7
Paweł Kochanek <pawel.kochanek@gmail.com>
2018-10-28 00:16:52 +0200

    First tests to cover some basics

--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -1,16 +1,36 @@
 import React from "react";
-import { render } from "react-testing-library";
-
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+import { render, fireEvent } from "react-testing-library";
 import { Game } from "../Game";
 
-test("react-testing-library works", () => {
-  expect(1 + 2).toBe(3);
-  const boardSizeX = 10;
-  const boardSizeY = 10;
-  const { getByTestId, container } = render(
-    <Game boardSizeX={boardSizeX} boardSizeY={boardSizeY} />
-  );
+const boardSizeX = 10;
+const boardSizeY = 10;
 
-  const gameTable = getByTestId("game-table");
-  expect(gameTable.children.length).toBe(1);
+describe('Game board', () => {
+    it('should render correctly', () => {
+        const { getByTestId } = render(
+            <Game boardSizeX={boardSizeX} boardSizeY={boardSizeY} />
+        );
+        const gameTable = getByTestId("game-table");
+        expect(gameTable).toBeVisible();
+    });
+
+    it('should have only one user tile', () => {
+        const { container } = render(
+            <Game boardSizeX={boardSizeX} boardSizeY={boardSizeY} />
+        );
+
+        const userTile = container.querySelectorAll('.has-user');
+        expect(userTile).toHaveLength(1);
+    });
+
+    it('should have 10 sprite tiles at the begining of the game', () => {
+        const { container } = render(
+            <Game boardSizeX={boardSizeX} boardSizeY={boardSizeY} />
+        );
+
+        const spriteTiles = container.querySelectorAll('.has-sprite');
+        expect(spriteTiles).toHaveLength(10);
+    });
 });
