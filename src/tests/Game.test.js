3259b6aa5e7e3fccf6c3536d0d427525370d9eb4
Paweł Kochanek <pawel.kochanek@gmail.com>
2018-11-01 00:22:38 +0100

    Final tests

--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -1,8 +1,8 @@
-import React from "react";
-import 'jest-dom/extend-expect'
-import 'react-testing-library/cleanup-after-each'
-import { render, fireEvent } from "react-testing-library";
-import { Game } from "../Game";
+import React from 'react';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+import { render, fireEvent } from 'react-testing-library';
+import { Game } from '../Game';
 
 const boardSizeX = 10;
 const boardSizeY = 10;
@@ -12,7 +12,7 @@ describe('Game board', () => {
         const { getByTestId } = render(
             <Game boardSizeX={boardSizeX} boardSizeY={boardSizeY} />
         );
-        const gameTable = getByTestId("game-table");
+        const gameTable = getByTestId('game-table');
         expect(gameTable).toBeVisible();
     });
 
@@ -44,9 +44,14 @@ describe('User movement', () => {
             <Game boardSizeX={boardSizeX} boardSizeY={boardSizeY} />
         );
         // It has to +1, as applying for has-user class happens in a loop, that is iterating from 0.
-        initialPosition = { x: Math.floor(boardSizeX / 2) + 1, y: Math.floor(boardSizeY / 2) + 1 };
+        initialPosition = {
+            x: Math.floor(boardSizeX / 2) + 1,
+            y: Math.floor(boardSizeY / 2) + 1
+        };
         board = getByTestId('game-table');
-        const userPosition = container.querySelector(`tr:nth-child(${initialPosition.y})>td:nth-child(${initialPosition.x})`);
+        const userPosition = container.querySelector(
+            `tr:nth-child(${initialPosition.y})>td:nth-child(${initialPosition.x})`
+        );
         expect(userPosition.classList.contains('has-user')).toBe(true);
     });
 
@@ -67,9 +72,7 @@ describe('User movement', () => {
             key: 'ArrowRight',
             keyCode: 39
         });
-        const newUserPosition = board.querySelector(
-            `tr:nth-child(${initialPosition.y})>td:nth-child(${initialPosition.x + 1})`
-        );
+        const newUserPosition = board.querySelector(`tr:nth-child(${initialPosition.y})>td:nth-child(${initialPosition.x + 1})`);
         expect(newUserPosition.classList.contains('has-user')).toBe(true);
     });
 
@@ -78,8 +81,7 @@ describe('User movement', () => {
             key: 'ArrowDown',
             keyCode: 40
         });
-        const newUserPosition = board.querySelector(
-            `tr:nth-child(${initialPosition.y + 1})>td:nth-child(${initialPosition.y})`
+        const newUserPosition = board.querySelector(`tr:nth-child(${initialPosition.y + 1})>td:nth-child(${initialPosition.y})`
         );
         expect(newUserPosition.classList.contains('has-user')).toBe(true);
     });
@@ -89,10 +91,101 @@ describe('User movement', () => {
             key: 'ArrowUp',
             keyCode: 38
         });
-        const newUserPosition = board.querySelector(
-            `tr:nth-child(${initialPosition.y - 1})>td:nth-child(${initialPosition.y})`
+        const newUserPosition = board.querySelector(`tr:nth-child(${initialPosition.y - 1})>td:nth-child(${initialPosition.y})`
         );
-        console.log(newUserPosition.innerHTML)
+        console.log(newUserPosition.innerHTML);
         expect(newUserPosition.classList.contains('has-user')).toBe(true);
+    });
+});
+
+describe('Move counter', () => {
+    it('should increase after valid step', () => {
+        const { getByTestId } = render(
+            <Game boardSizeX={boardSizeX} boardSizeY={boardSizeY} />
+        );
+        const board = getByTestId('game-table');
+        fireEvent.keyDown(board, {
+            key: 'ArrowUp',
+            keyCode: 38
+        });
+        const moveCounter = getByTestId('moveCounter');
+        expect(Number(moveCounter.innerHTML)).toBe(1);
+        fireEvent.keyDown(board, {
+            key: 'ArrowUp',
+            keyCode: 38
+        });
+        expect(Number(moveCounter.innerHTML)).toBe(2);
+    });
+
+    it('should not increase move counter, when there is no place to move up', () => {
+
+        // boardSizeX is so big, because has-sprite could start in the same position that
+        // has-user. Then even that move was blocked, sprite is "collected" so game finished
+        // before registering user didn't moved. There is still 1% chance this will crash.
+        const { getByTestId } = render(
+            <Game boardSizeX={100} boardSizeY={1} />
+        );
+        const board = getByTestId('game-table');
+        fireEvent.keyDown(board, {
+            key: 'ArrowUp',
+            keyCode: 38
+        });
+        const moveCounter = getByTestId('moveCounter');
+        expect(Number(moveCounter.innerHTML)).toBe(0);
+    });
+
+    it('should not increase move counter, when there is no place to move down', () => {
+        const { getByTestId } = render(
+            <Game boardSizeX={100} boardSizeY={1} />
+        );
+        const board = getByTestId('game-table');
+        fireEvent.keyDown(board, {
+            key: 'ArrowDown',
+            keyCode: 40
+        });
+        const moveCounter = getByTestId('moveCounter');
+        expect(Number(moveCounter.innerHTML)).toBe(0);
+    });
+
+    it('should not increase move counter, when there is no place to move left', () => {
+        const { getByTestId } = render(<Game boardSizeX={1} boardSizeY={2} />);
+        const board = getByTestId('game-table');
+        fireEvent.keyDown(board, {
+            key: 'ArrowLeft',
+            keyCode: 37
+        });
+        const moveCounter = getByTestId('moveCounter');
+        expect(Number(moveCounter.innerHTML)).toBe(0);
+    });
+
+    it('should not increase move counter, when there is no place to move to right', () => {
+        const { getByTestId } = render(<Game boardSizeX={1} boardSizeY={2} />);
+        const board = getByTestId('game-table');
+        fireEvent.keyDown(board, {
+            key: 'ArrowRight',
+            keyCode: 39
+        });
+        const moveCounter = getByTestId('moveCounter');
+        expect(Number(moveCounter.innerHTML)).toBe(0);
+    });
+
+    it('should show the score after game end', () => {
+        const { getByTestId } = render(
+            <Game boardSizeX={2} boardSizeY={1} />
+        );
+        const board = getByTestId('game-table');
+
+        // because has-sprite can be on has-user tail, we have to move two directions, to get
+        // the sprite that starts on initial user position
+        fireEvent.keyDown(board, {
+            key: 'ArrowLeft',
+            keyCode: 37
+        });
+        fireEvent.keyDown(board, {
+            key: 'ArrowRight',
+            keyCode: 39
+        });
+        const moveCounter = getByTestId('moveCounter');
+        expect(Number(moveCounter.innerHTML)).toBeGreaterThanOrEqual(1);
     });
 });
