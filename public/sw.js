9432befb09286d3dbfa15a78623fba90b21d0529
RuthwikHM <41834689+RuthwikHM@users.noreply.github.com>
2019-10-01 21:27:46 +0530

    Added service worker support
    Added support for caching using service worker.

---
+++ public/sw.js
@@ -0,0 +1,75 @@
+//The script responsible for caching of assets.
+//Update cache version if any changes are made to assets.
+const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
+//List of assets to be cached.
+const assets = [
+  '/',
+  '/public/index.html',
+  '/src/index.js',
+  '/src/Game.js',
+  '/src/styles.css',
+  '/public/images/board-background.png',
+  '/public/images/sweet-1.png',
+  '/public/images/sweet-2.png',
+  '/public/images/sweet-3.png',
+  '/public/images/user.png'
+];
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+// install event
+self.addEventListener('install', evt => {
+  console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// fetch events
+self.addEventListener('fetch', evt => {
+  if(evt.request.url.indexOf('trickortreat.surge.sh') === -1){
+    evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetch(evt.request).then(fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone());
+            // check cached items size
+            limitCacheSize(dynamicCacheName, 15);
+            return fetchRes;
+          })
+        });
+      }).catch(() => {
+        if(evt.request.url.indexOf('.html') > -1){
+          return caches.match('/pages/fallback.html');
+        } 
+      })
+    );
+  }
+});
