3dac9dfef63ba8d14edd76873bb23f3e85755fc6
sohammondal <soham.pf.mondal@gmail.com>
2019-10-02 00:32:34 +0530

    remove comment, refactor renderScoreboard function to get state date from within function

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -1,13 +1,6 @@
 import React from "react";
 import "./scoreboard.css";
 
-/* 
-    Read saved user moves from localStorage
-    Render Scoreboard
-    
-    ** Contributed by Soham Mondal <contact@sohammondal.com> **
-  */
-
 class Scoreboard extends React.Component {
   constructor(props) {
     super(props);
@@ -28,19 +21,20 @@ class Scoreboard extends React.Component {
     }
   }
 
-  renderScoreboard(userMovesArray) {
+  renderScoreboard() {
+    const { userMoves } = this.state;
     return (
       <div className="scoreboard">
         <div className="scoreboard-title">Scoreboard</div>
         <table>
           <tbody>
-            {userMovesArray.map(({ name, moves }, index) => {
+            {userMoves.map(({ name, moves }, index) => {
               let color = "#FFFFF";
               if (index === 0) {
                 //green - top score - least moves
                 color = "#01D28E";
               }
-              if (index > 0 && index === userMovesArray.length - 1) {
+              if (index > 0 && index === userMoves.length - 1) {
                 //red - worst score - most moves
                 color = "#FA1D2F";
               }
@@ -64,7 +58,7 @@ class Scoreboard extends React.Component {
   }
   render() {
     return this.state.userMoves
-      ? this.renderScoreboard(this.state.userMoves)
+      ? this.renderScoreboard()
       : null;
   }
 }
