a9845b4a6124f35a8107484ea2f1c67aec6fd285
sohammondal <soham.pf.mondal@gmail.com>
2019-10-01 15:28:12 +0530

    change scoreboard from list to table

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -2,63 +2,70 @@ import React from "react";
 import "./scoreboard.css";
 
 /* 
-    Read moves from localStorage
+    Read saved user moves from localStorage
     Render Scoreboard
-    Contributed by Soham Mondal <contact@sohammondal.com>
+    
+    ** Contributed by Soham Mondal <contact@sohammondal.com> **
   */
 
 class Scoreboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      moves: null
+      userMoves: null
     };
   }
 
   componentDidMount() {
-    this.getMovesFromLocalStorage();
+    this.getUserMovesFromLocalStorage();
   }
-  getMovesFromLocalStorage() {
-    let savedMoves = localStorage.getItem("moves");
-    if (savedMoves) {
-      savedMoves = savedMoves.split(",").sort();
-      this.setState({ moves: savedMoves });
+  getUserMovesFromLocalStorage() {
+    let savedUserMoves = localStorage.getItem("userMoves");
+    if (savedUserMoves) {
+      savedUserMoves = JSON.parse(savedUserMoves);
+      savedUserMoves.sort((user1, user2) => user1.moves - user2.moves);
+      this.setState({ userMoves: savedUserMoves });
     }
   }
 
-  renderScoreboard(moves) {
+  renderScoreboard(userMovesArray) {
     return (
       <div className="scoreboard">
-        <div className="scoreboard-title">Past Scores</div>
-        <ul className="scoreboard-scores-list">
-          {moves.map((move, index) => {
-            let color = "#FFFFF";
-            if (index === 0) {
-              //green - top score - least moves
-              color = "#01D28E";
-            }
-            if (index > 0 && index === moves.length - 1) {
-              //red - worst score - most moves
-              color = "#FA1D2F";
-            }
-            return (
-              <li
-                key={index}
-                className="scoreboard-score-list-item"
-                style={{
-                  color
-                }}
-              >
-                {move}
-              </li>
-            );
-          })}
-        </ul>
+        <div className="scoreboard-title">Scoreboard</div>
+        <table>
+          <tbody>
+            {userMovesArray.map(({ name, moves }, index) => {
+              let color = "#FFFFF";
+              if (index === 0) {
+                //green - top score - least moves
+                color = "#01D28E";
+              }
+              if (index > 0 && index === userMovesArray.length - 1) {
+                //red - worst score - most moves
+                color = "#FA1D2F";
+              }
+              return (
+                <tr
+                  key={index}
+                  style={{
+                    color
+                  }}
+                >
+                  <td>{index + 1}.</td>
+                  <td>{name}</td>
+                  <td>{moves}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
   render() {
-    return this.state.moves ? this.renderScoreboard(this.state.moves) : null;
+    return this.state.userMoves
+      ? this.renderScoreboard(this.state.userMoves)
+      : null;
   }
 }
 
