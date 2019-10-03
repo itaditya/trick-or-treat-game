5253140b183d72380c19c439947d23aafe09155a
Rajat Verma <rajatverma5885045@gmail.com>
2019-10-04 01:57:41 +0530

    Update Controls.js

--- src/Controls.js
+++ src/Controls.js
@@ -7,29 +7,53 @@ class Controls extends Component {
       <Fragment>
         <div className="controls">
           <div className="controls-title">Instructions to Play</div>
-          <ol>
+          <ul>
 
-            <li>1. Press <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-            <path fill="none" d="M0 0h24v24H0z" />
-            <path d="M8 12l6-6v12z" fill="rgba(255,255,255,1)" />
-            </svg> to Move Left.</li>
+            <li>
+              <p className="instruction">
+                Press
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path d="M8 12l6-6v12z" fill="rgba(255,255,255,1)" />
+                </svg>
+                to Move Left.
+              </p>
+            </li>
 
-            <li>2. Press <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-            <path fill="none" d="M0 0h24v24H0z" />
-            <path d="M16 12l-6 6V6z" fill="rgba(255,255,255,1)" />
-            </svg> to Move Right.</li>
+            <li>
+              <p className="instruction">
+                Press
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path d="M16 12l-6 6V6z" fill="rgba(255,255,255,1)" />
+                </svg>
+                to Move Right.
+              </p>
+            </li>
 
-            <li>3. Press <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-            <path fill="none" d="M0 0h24v24H0z" />
-            <path d="M12 8l6 6H6z" fill="rgba(255,255,255,1)" />
-            </svg> to Move Up.</li>
+            <li>
+              <p className="instruction">
+                Press 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path d="M12 8l6 6H6z" fill="rgba(255,255,255,1)" />
+                </svg>
+                to Move Up.
+              </p>
+            </li>
 
-            <li>4. Press <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-            <path fill="none" d="M0 0h24v24H0z" />
-            <path d="M12 16l-6-6h12z" fill="rgba(255,255,255,1)" />
-            </svg> to Move Down.</li>
+            <li>
+              <p className="instruction">
+                Press 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path d="M12 16l-6-6h12z" fill="rgba(255,255,255,1)" />
+                </svg>
+                to Move Down.
+              </p>
+            </li>
 
-          </ol>
+          </ul>
         </div>
         <p className="note">Note:&nbsp;To win get all the candies <br/> in the minimum possible moves.</p>
       </Fragment>
