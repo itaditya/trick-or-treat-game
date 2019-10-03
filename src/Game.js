7c121e1b91af607f04f28fa2a1bf765836e253a2
Rajat Verma <rajatverma5885045@gmail.com>
2019-10-04 02:18:24 +0530

    made instructions section (#46)
    Added instructions to play the game

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment, createRef } from 'react';
 
 import { ScoreBoard } from './ScoreBoard';
+import { Controls } from './Controls';
+
 
 class Game extends Component {
   constructor(props) {
@@ -311,7 +313,10 @@ class Game extends Component {
                 </button>
               </div>
             </div>
-            <ScoreBoard />
+            <div>
+              <Controls />
+              <ScoreBoard />
+            </div>
           </Fragment>
         )}
       </main>
