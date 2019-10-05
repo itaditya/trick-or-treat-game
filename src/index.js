f9086975547b367111df5a2ae517babf67817326
Agastya Chandrakant <acagastya@outlook.com>
2019-10-05 19:58:12 +0530

    Adding option for size as mentioned in #51

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 import { Game } from "./Game";
 
-const App = function() {
-  const boardSizeX = 10;
-  const boardSizeY = 10;
+import getUrlParams from './getUrlParams';
+
+const App = function () {
+
+  let size = parseInt(getUrlParams('size'), 10);
+
+  if (isNaN(size)) size = 10;
+  else if (size < 4) size = 4;
+  else if (size > 10) size = 10;
+
+  const boardSizeX = size;
+  const boardSizeY = size;
 
   return (
     <div className="App">
