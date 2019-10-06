391c8e2b38c12578667a2c28156e24e41e7bce79
abc <rajat2502@users.noreply.github.com>
2019-10-06 23:59:23 +0530

    details tag added to instructions list

--- src/Controls.js
+++ src/Controls.js
@@ -3,55 +3,58 @@ import './controls.css';
 
 class Controls extends Component {
   render() {
+    const detailState = localStorage.getItem('userMoves') == null ? true : false;
     return (
       <Fragment>
         <div className="controls">
-          <div className="controls-title">Instructions to Play</div>
-          <ul className="instructions-list">
-            <li>
-              <p className="instruction">
-                Press
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-                  <path fill="none" d="M0 0h24v24H0z" />
-                  <path d="M8 12l6-6v12z" fill="rgba(255,255,255,1)" />
-                </svg>
-                to Move Left.
-              </p>
-            </li>
+          <details className="details" open={ detailState }>
+            <summary className="controls-title summary">Instructions to Play</summary>
+            <ul className="instructions-list">
+              <li>
+                <p className="instruction">
+                  Press
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path d="M8 12l6-6v12z" fill="rgba(255,255,255,1)" />
+                  </svg>
+                  to Move Left.
+                </p>
+              </li>
 
-            <li>
-              <p className="instruction">
-                Press
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-                  <path fill="none" d="M0 0h24v24H0z" />
-                  <path d="M16 12l-6 6V6z" fill="rgba(255,255,255,1)" />
-                </svg>
-                to Move Right.
-              </p>
-            </li>
+              <li>
+                <p className="instruction">
+                  Press
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path d="M16 12l-6 6V6z" fill="rgba(255,255,255,1)" />
+                  </svg>
+                  to Move Right.
+                </p>
+              </li>
 
-            <li>
-              <p className="instruction">
-                Press
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-                  <path fill="none" d="M0 0h24v24H0z" />
-                  <path d="M12 8l6 6H6z" fill="rgba(255,255,255,1)" />
-                </svg>
-                to Move Up.
-              </p>
-            </li>
+              <li>
+                <p className="instruction">
+                  Press
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path d="M12 8l6 6H6z" fill="rgba(255,255,255,1)" />
+                  </svg>
+                  to Move Up.
+                </p>
+              </li>
 
-            <li>
-              <p className="instruction">
-                Press
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-                  <path fill="none" d="M0 0h24v24H0z" />
-                  <path d="M12 16l-6-6h12z" fill="rgba(255,255,255,1)" />
-                </svg>
-                to Move Down.
-              </p>
-            </li>
-          </ul>
+              <li>
+                <p className="instruction">
+                  Press
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path d="M12 16l-6-6h12z" fill="rgba(255,255,255,1)" />
+                  </svg>
+                  to Move Down.
+                </p>
+              </li>
+            </ul>
+          </details>
         </div>
         <p className="note">
           Note:&nbsp;To win get all the candies <br /> in the minimum possible moves.
