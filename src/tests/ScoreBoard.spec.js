440ef15cb8757058b125a7fafff204b0b11454d5
devTeaa <38621036+devTeaa@users.noreply.github.com>
2019-10-06 21:44:37 +0700

    Update as requested https://github.com/itaditya/trick-or-treat-game/pull/57#discussion_r331778491 and https://github.com/itaditya/trick-or-treat-game/pull/57#discussion_r331772259

--- src/tests/ScoreBoard.spec.js
+++ src/tests/ScoreBoard.spec.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, getNodeText } from 'react-testing-library';
 import { ScoreBoard } from '../ScoreBoard';
 
-const originalLocalStorage = global.window;
+const originalLocalStorage = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+};
 
 const mockLocalStorage = {
   getItem: function (key) {
@@ -18,21 +22,23 @@ const mockLocalStorage = {
   clear: jest.fn()
 }
 
-beforeAll(() => {
-  global.localStorage = mockLocalStorage;
+beforeEach(() => {
+  global.window.localStorage = mockLocalStorage;
 })
 
 describe('ScoreBoard component', () => {
-  it('should render three items in list if three items are stored in localStorage', () => {
+  it('should render component as null', () => {
+
+    global.window.localStorage = originalLocalStorage;
+
     const { container } = render(<ScoreBoard />);
-    expect(container.querySelectorAll('li')).toHaveLength(3)
+    expect(container.firstChild).toBeNull()
   }) 
 
   it('should render three scores as ordered list if three items are stored in localStorage', () => {
     const { container } = render(<ScoreBoard />);
-    expect(container.querySelectorAll('li')[0].innerText).toEqual('Andy: 8 moves')
-    expect(container.querySelectorAll('li')[1].innerText).toEqual('Bill: 10 moves')
-    expect(container.querySelectorAll('li')[1].innerText).toEqual('John: 20 moves')
+    expect(container.querySelectorAll('li')[0].textContent).toEqual('Andy: 8 moves')
+    expect(container.querySelectorAll('li')[1].textContent).toEqual('Bill: 10 moves')
+    expect(container.querySelectorAll('li')[2].textContent).toEqual('John: 20 moves')
   })
 });
-
