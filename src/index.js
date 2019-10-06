1d790f3585fab78becbcddc72c7e7192c815bbf1
Agastya Chandrakant <acagastya@outlook.com>
2019-10-06 14:38:11 +0530

    Handling size from parameter in a function

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import "./styles.css";
 import { Game } from "./Game";
 import { BOARD_SIZE_MAX, BOARD_SIZE_MIN } from './constants';
 
+function getBoardSize() {
+  const strQuery = window.location.search;
+  const paramsQuery = new URLSearchParams(strQuery);
+  const paramSize = paramsQuery.get('size');
+  return parseInt(paramSize, 10) || BOARD_SIZE_MAX;
+}
+
 const App = function () {
-  // eslint-disable-next-line
-  let size = parseInt(new URLSearchParams(location.search).get('size'), 10);
-  if (isNaN(size)) size = BOARD_SIZE_MAX;
+  let size = getBoardSize();
   size = Math.max(BOARD_SIZE_MIN, size);
   size = Math.min(BOARD_SIZE_MAX, size);
 
