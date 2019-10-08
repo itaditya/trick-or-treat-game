a61351a6448b19fa9f28ae6bf81d9b48fdcd4707
duy quang <hyouka266@gmail.com>
2019-10-08 23:12:42 +0700

    Added background audio (#21)
    * Added back ground audio
    
    * Added lazy load
    
    * merge
    
    * update code and compress mp3
    
    * fix conflicts
    
    * format

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles.css';
 import { Game } from './Game';
+const BackgroundAudio = lazy(() => import('./BackgroundAudio'));
 
 const App = function() {
   const boardSizeX = 10;
@@ -10,6 +11,9 @@ const App = function() {
 
   return (
     <div className="App">
+      <Suspense fallback={<div>Loading...</div>}>
+        <BackgroundAudio />
+      </Suspense>
       <Game boardSizeX={boardSizeX} boardSizeY={boardSizeY} />
     </div>
   );
